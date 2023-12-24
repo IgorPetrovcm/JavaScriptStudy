d6f9e7358e0f5a71fc272c79fbe7221446588906
igoradm <igorpetrov42ru@gmail.com>
2023-12-24 21:17:51 +0300

    prototype first general practic start

--- Practics/GeneralPractic/js/create_row.js
+++ Practics/GeneralPractic/js/create_row.js
@@ -1,4 +1,31 @@
 function btn_createRow()
 {
-    
+    let table = document.getElementById("table1");
+
+    let tr = document.createElement('tr');
+    tr.id = "trChild";
+    tr.onclick = tr_click;
+    if (table.children.length == 0){
+        let th = document.createElement('th');
+        th.innerText = "Message";
+
+        tr.appendChild(th);
+        table.appendChild(tr);
+        return;
+    }
+
+    let td = document.createElement('td');
+    td.innerText = "hello";
+
+    tr.appendChild(td);
+    table.appendChild(tr);
 }
+function tr_click()
+{
+    alert("You click to tr tag");
+}
+let button = document.getElementById("btn_createRow");
+
+button.addEventListener("click",btn_createRow);
+
+
